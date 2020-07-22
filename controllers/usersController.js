4f67c7354c78a86b6c7caaf06055785b3cce119c
nathanjh-28 <nathan@nathanjharris.com>
2020-07-21 18:51:30 -0600

    added post route, seed file, user index ejs added and it works

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,7 +5,20 @@ const router = express.Router();
 const db = require('../models');
 
 router.get('/', (req,res)=>{
-    res.render('users/user-index');
+    db.User.find({},(err,users)=>{
+        if(err)console.log(err);
+        res.render('users/user-index',{
+            users: users,
+        });
+    })
+});
+
+router.post('/', (req,res)=>{
+    db.User.create(req.body,(err,newUser)=>{
+        if(err)console.log(err);
+        console.log(newUser);
+    })
+    res.redirect('/');
 });
 
 
