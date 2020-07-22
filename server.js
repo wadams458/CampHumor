ec7b8c16b3523bcb5e69c806333bb2150dcd6a8b
roberto borberg <robertoborberg@gmail.com>
2020-07-22 15:05:40 -0400

    initial layouts

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
 // ------------- REQUIREMENTS ------------- //
-
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 const PORT = 4000;
-
+const db = require('./models');
 // ------------- CONTROLLERS ------------- //
-
 //controller
 const usersCtrl = require('./controllers/usersController');
-
-
 //config views
 app.set('view engine', 'ejs');
-
-
 // ------------- MIDDLEWARE ------------- //
-
 app.use(express.static(`${__dirname}/public`));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:false}));
-
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url} ${new Date().toLocaleTimeString()}`);
     next();
     });
-
 // ------------- ROUTES ------------- //
-
 // Home (Root) Route
 app.get('/', (req, res) => {
     res.render('index');
     });
-
+app.post('/', (req,res)=>{
+    db.User.create(req.body,(err,newUser)=>{
+        if(err){
+            res.render('index.ejs',{
+                msg: err,
+            });
+        }else {
+        console.log(err);
+        console.log(newUser);
+        res.render('index', {
+            msg: 'Email Address Added!'
+        });
+        }
+    })
+});
 // User Route
 app.use('/users', usersCtrl);
-
-
-
 // ------------- SERVER LISTENER ------------- //
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
